ee9d1280b9f2927cd8fbab64146040eb08558d33
dev-code-create <ayushkankale04@gmail.com>
2025-01-18 17:29:56 +0530

    Created Input Box

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,7 +6,7 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Box minH={"100vh"}  bg={useColorModeValue("gray.100", "gray.900"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
